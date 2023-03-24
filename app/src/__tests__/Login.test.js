14cf7c8b1a4bb78b223d71efc4a37e3dcd0417a8
marcelamchdo <arqmrcl@gmail.com>
2023-03-24 13:01:09 -0300

    test: verify the existence of E-mail, Senha and Entrar fields in LoginForm

--- app/src/__tests__/Login.test.js
+++ app/src/__tests__/Login.test.js
@@ -1,6 +1,15 @@
-import LoginForm from "../components/LoginForm";
-import {render, screen} from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import LoginForm from '../components/LoginForm';
 
-    test("test_username_change: tests that the username input field updates its value correctly when the user types.", () => {
-      render(<LoginForm />)
-    });
+describe('LoginForm', () => {
+  it('verify the existence of E-mail, Senha and Entrar fields', () => {
+    render(<LoginForm/>);
+    const usernameInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const submitButton = screen.getByText('Entrar');
+
+    fireEvent.change(usernameInput, { target: { value: 'example@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    fireEvent.click(submitButton);
+  });
+});
