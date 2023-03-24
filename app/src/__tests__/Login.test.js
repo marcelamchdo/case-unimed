bdf6f3fa8c7181d7779806e53a1d84710f32631f
marcelamchdo <arqmrcl@gmail.com>
2023-03-24 13:31:17 -0300

    test: verify if Entrar button is enabled and disabled

--- app/src/__tests__/Login.test.js
+++ app/src/__tests__/Login.test.js
@@ -12,4 +12,26 @@ describe('LoginForm', () => {
     fireEvent.change(passwordInput, { target: { value: 'password123' } });
     fireEvent.click(submitButton);
   });
+
+  it('verify Entrar button is disabled', () => {
+    render(<LoginForm/>);
+    const usernameInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const submitButton = screen.getByText('Entrar');
+
+    fireEvent.change(usernameInput, { target: { value: 'invalidemail' } });
+    fireEvent.change(passwordInput, { target: { value: '' } });
+    expect(submitButton).not.toBeEnabled()
+  });
+
+  it('verify Entrar button is enabled', () => {
+    render(<LoginForm/>);
+    const usernameInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const submitButton = screen.getByText('Entrar');
+
+    fireEvent.change(usernameInput, { target: { value: 'example@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    expect(submitButton).toBeEnabled()
+  })
 });
