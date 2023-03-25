ce50ab2df4bb0a24ec24fe580b34ec1a04dab358
marcelamchdo <arqmrcl@gmail.com>
2023-03-25 18:39:33 -0300

    fix: fix test LoginForm

--- app/src/__tests__/Login.test.js
+++ app/src/__tests__/Login.test.js
@@ -8,13 +8,13 @@ const mockStore = configureMockStore();
 
 describe('LoginForm', () => {
   it('should render without crashing', () => {
-    const store = mockStore({user: {message:''}}); // pass initial state as an object
+    const store = mockStore({user: {message:''}});
     render(
       <Provider store={store}>
         <LoginForm/>
       </Provider>
     );
-        const usernameInput = screen.getByLabelText('E-mail');
+    const usernameInput = screen.getByLabelText('E-mail');
     const passwordInput = screen.getByLabelText('Senha');
     const submitButton = screen.getByText('Entrar');
 
@@ -23,45 +23,62 @@ describe('LoginForm', () => {
     fireEvent.click(submitButton);
   });
   
+  it('verify the existence of E-mail, Senha and Entrar fields', () => {
+    const store = mockStore({user: {message:''}});
+    render(
+      <Provider store={store}>
+        <LoginForm/>
+      </Provider>
+    );
+    const usernameInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const submitButton = screen.getByText('Entrar');
 
+    fireEvent.change(usernameInput, { target: { value: 'example@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    fireEvent.click(submitButton);
+  });
 
-  // })
-  // it('verify the existence of E-mail, Senha and Entrar fields', () => {
-  //   render(<LoginForm/>);
-  //   const usernameInput = screen.getByLabelText('E-mail');
-  //   const passwordInput = screen.getByLabelText('Senha');
-  //   const submitButton = screen.getByText('Entrar');
+  it('verify Entrar button is disabled', () => {
+    const store = mockStore({user: {message:''}});
+    render(
+      <Provider store={store}>
+        <LoginForm/>
+      </Provider>
+    );
+    const usernameInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const submitButton = screen.getByText('Entrar');
 
-  //   fireEvent.change(usernameInput, { target: { value: 'example@example.com' } });
-  //   fireEvent.change(passwordInput, { target: { value: 'password123' } });
-  //   fireEvent.click(submitButton);
-  // });
+    fireEvent.change(usernameInput, { target: { value: 'invalidemail' } });
+    fireEvent.change(passwordInput, { target: { value: '' } });
+    expect(submitButton).not.toBeEnabled()
+  });
 
-  // it('verify Entrar button is disabled', () => {
-  //   render(<LoginForm/>);
-  //   const usernameInput = screen.getByLabelText('E-mail');
-  //   const passwordInput = screen.getByLabelText('Senha');
-  //   const submitButton = screen.getByText('Entrar');
+  it('verify Entrar button is enabled', () => {
+    const store = mockStore({user: {message:''}});
+    render(
+      <Provider store={store}>
+        <LoginForm/>
+      </Provider>
+    );
+    const usernameInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const submitButton = screen.getByText('Entrar');
 
-  //   fireEvent.change(usernameInput, { target: { value: 'invalidemail' } });
-  //   fireEvent.change(passwordInput, { target: { value: '' } });
-  //   expect(submitButton).not.toBeEnabled()
-  // });
+    fireEvent.change(usernameInput, { target: { value: 'example@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    expect(submitButton).toBeEnabled()
+  });
 
-  // it('verify Entrar button is enabled', () => {
-  //   render(<LoginForm/>);
-  //   const usernameInput = screen.getByLabelText('E-mail');
-  //   const passwordInput = screen.getByLabelText('Senha');
-  //   const submitButton = screen.getByText('Entrar');
-
-  //   fireEvent.change(usernameInput, { target: { value: 'example@example.com' } });
-  //   fireEvent.change(passwordInput, { target: { value: 'password123' } });
-  //   expect(submitButton).toBeEnabled()
-  // });
-
-  // it('verify Não tenho conta button is always enabled', () => {
-  //   render(<LoginForm/>);
-  //   const submitButton = screen.getByText('Não tenho conta');
-  //   expect(submitButton).toBeEnabled()
-  // })
+  it('verify Não tenho conta button is always enabled', () => {
+    const store = mockStore({user: {message:''}});
+    render(
+      <Provider store={store}>
+        <LoginForm/>
+      </Provider>
+    );
+    const submitButton = screen.getByText('Não tenho conta');
+    expect(submitButton).toBeEnabled()
+  })
 });
