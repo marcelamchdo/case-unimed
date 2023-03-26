7839454efa8d7c56267e989b14c5e454b04a7fe7
marcelamchdo <arqmrcl@gmail.com>
2023-03-25 22:56:13 -0300

    feat: add crud api

---
+++ app/src/database/api/services/UsersService.js
@@ -0,0 +1,50 @@
+import { Api } from "../ApiConfig"
+
+const getAll = async () => {
+  try {
+    const { data } = await Api().get('/users');
+    return data; 
+  } catch (error) {
+    return error.stack
+  }
+}
+const getById = async (id) => {
+  try {
+    const { data } = await Api().get(`/users/${id}`);
+    return data; 
+  } catch (error) {
+    return error.stack
+  }
+}
+const createUser = async (usersToCreate) => {
+  try {
+    const { data } = await Api().post('/users', usersToCreate);
+    return data; 
+  } catch (error) {
+    return error.stack
+  }
+}
+const updateById = async (id, dataToUpdate) => {
+  try {
+    const { data } = await Api().put(`/users/${id}`, dataToUpdate);
+    return data; 
+  } catch (error) {
+    return error.stack
+  }
+}
+const deleteById = async (id) => {
+  try {
+    await Api().delete(`/users/${id}`);
+    return undefined; 
+  } catch (error) {
+    return error.stack
+  }
+}
+
+export const UsersService = { 
+  getAll,
+  createUser,
+  getById,
+  updateById,
+  deleteById,
+}
