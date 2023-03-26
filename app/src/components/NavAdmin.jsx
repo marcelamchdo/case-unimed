51f71e39b1e393e56a1f310f9d7f17d8b0b0e8d1
marcelamchdo <arqmrcl@gmail.com>
2023-03-26 11:07:00 -0300

    style: add style in users page

--- app/src/components/NavAdmin.jsx
+++ app/src/components/NavAdmin.jsx
@@ -16,11 +16,11 @@ const NavAdmin = () => {
   
     <div className="icons">
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#9A9A9A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
       </div>
 
       <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#9A9A9A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
       </div>
       <div>
       <img src={perfil} className="perfil" alt="perfil"/>
--- app/src/pages/Users.jsx
+++ app/src/pages/Users.jsx
@@ -1,13 +1,38 @@
 import NavAdmin from "../components/NavAdmin";
 import Sidebar from "../components/SiderbarAdmin";
 import Users from "../components/Users";
+import "../styles/PagesStyles/Users.scss"
 
 const UsersAdmin = () => {
   return (
-    <div>
-      <NavAdmin />
-      <Sidebar />
-      <Users />
+    <div className="welcome">
+      <div className="sidebarAdmin">
+        <Sidebar />
+      </div>
+      <div className="columnWelcome">
+        <NavAdmin />
+        <div className="usersHeader">
+          <h1 className="usersText">
+            Usuários
+          </h1>
+
+          <div className="searchBox">
+            <span className="feather2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#DADADA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+            </span>
+            <input 
+            className="searchInput"
+            placeholder="Buscar usuário"
+            />
+
+          <button className="btnAddUser">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
+            Adicionar usuário
+          </button>
+          </div>
+        </div>
+        <Users />
+      </div>
     </div>
   )}
 
